1d88fa17d4b0f5d864a65ea4be5c24c89fb3fde3
Hamidou Diallo <dio.hamidou@gmail.com>
2019-10-09 05:33:36 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+class School
+  attr_accessor :roster, :grade
+  def initialize(name)
+    @name = name
+    @roster = Hash.new
+  end
+  def roster
+    @roster
+  end
+  def add_student(student_name,grade)
+    if roster.keys.include?(grade)
+      roster[grade] << student_name
+    else
+      roster[grade] = Array.new
+      roster[grade] <<  student_name
+    end
+  end
+  
+  def grade(student_grade)
+    if roster.keys.include?(student_grade)
+      roster[student_grade]
+    end
+  end
+  
+  def sort
+    roster.each do |grade,students|
+      students.sort!
+    end
+  end
+end
+
+
