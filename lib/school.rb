2a42247bfb02566a8a799c68bab1b31ec7f9fe98
RubenV-Dev <r.will16@hotmail.com>
2019-10-09 16:04:37 -0400

    1

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,7 @@ class School
         @roster = roster
     end
 
-    def roster
+    def self.roster
         {}
     end
 
@@ -19,6 +19,5 @@ class School
         @roster[grade] = []
         @roster[grade] << student
         roster = @roster
-        binding.pry
     end
 end
