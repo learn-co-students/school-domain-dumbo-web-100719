56b63d5cff33167ff846843f3b1265d757f627b6
RubenV-Dev <r.will16@hotmail.com>
2019-10-09 16:10:11 -0400

    1

--- lib/school.rb
+++ lib/school.rb
@@ -6,12 +6,12 @@ class School
 
     def initialize(name)
         @name = name
+        @roster = {}
         roster
-        @roster = roster
     end
-
+    
     def roster
-        {}
+        @roster
     end
 
     def add_student(student, grade)
